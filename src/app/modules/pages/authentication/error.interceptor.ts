dac27a1ec7195c152525f2a3be24fe95b6b8ef13
murdavron <murdavron@gmail.com>
2024-09-21 16:21:46 +0500

    integration auth login and logout

--- src/app/modules/pages/authentication/error.interceptor.ts
+++ src/app/modules/pages/authentication/error.interceptor.ts
@@ -3,14 +3,26 @@ import { Router } from '@angular/router';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { AuthService } from "@app/modules/pages/authentication/auth.service";
+import { LocalStorageService } from "@shared/services/storages/local-storage.service";
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private _router: Router, private _authenticationService: AuthService) {}
+  constructor(
+    private _router: Router,
+    private _localStorage: LocalStorageService,
+  ) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this._localStorage.get('token');
+    if (token) {
+      const modifiedHeaders = request.headers.append('Authorization', `Token ${token}`);
+      request = request.clone({
+        headers: modifiedHeaders,
+        withCredentials: true,
+      });
+    }
+
     return next.handle(request).pipe(
       catchError(err => {
         if ([401, 403].indexOf(err.status) !== -1) {
