57d5cfdf22b8a7ac2949c162c61040e793490ab5
archon1999 <nazarbek.baltabaev@gmail.com>
2024-09-21 15:50:36 +0500

    auth

--- src/app/modules/pages/authentication/auth.service.ts
+++ src/app/modules/pages/authentication/auth.service.ts
@@ -1,25 +1,42 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, of } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, of, tap } from 'rxjs';
 import { User } from '@app/modules/pages/authentication/user';
 import { ApiService } from '@shared/services/api.service';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../../environments/environment";
+import { catchError } from "rxjs/operators";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  protected _http = inject(HttpClient);
   private currentUserSubject = new BehaviorSubject<User>(null);
   public currentUser = this.currentUserSubject.asObservable();
 
-  constructor(private api: ApiService,) {}
+  constructor(private api: ApiService) {}
 
   public get currentUserValue(): User {
     return this.currentUserSubject.value;
   }
 
   getMe() {
-    return of(null);
+    return this.api.get('me').pipe(
+      tap((user: User) => {
+        if (user) {
+          this.currentUserSubject.next(user);
+        } else {
+          this.currentUserSubject.next(null);
+        }
+      }),
+      catchError(err => of(null))
+    );
   }
 
   login(username: string, password: string) {
-    return of(null);
+    const token = btoa(`${username}:${password}`);
+    const headers = { 'Authorization': `Basic ${token}` };
+    return this._http
+      .post<any>(`${environment.apiUrl}/api/login/`, {}, { headers: headers })
+      .pipe(tap(user => this.currentUserSubject.next(user)));
   }
 
   logout() {
