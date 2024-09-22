2ca163f93e608049179d5016798065571918372a
archon1999 <nazarbek.baltabaev@gmail.com>
2024-09-22 09:03:28 +0500

    [UPDATE] i18n

--- src/app/i18n/en.ts
+++ src/app/i18n/en.ts
@@ -1,13 +1,36 @@
 export const locale = {
-  lang: 'en',
+  lang: 'uz',
   data: {
     Menu: {
       Home: 'Home',
+      Problems: 'Problems',
+      Contests: 'Contests',
     },
     Login: 'Login',
     Logout: 'Logout',
     PageTitle: {
       Home: 'Home',
+    },
+    LoginText: 'Iltimos avtorizatsiyadan oʻting va sayohatni davom ettiring',
+    WelcomePage: 'Xush kelibsiz',
+    LoginSuccessText: 'Tizimga muvaffaqiyatli kirdingiz',
+    SolvedProblems: 'Ishlangan masalalar',
+    Status1: 'Ishlagan',
+    Status2: 'Ishlolmagan',
+    Status3: 'Urinish qilmagan',
+    Status4: 'Tizimga kirmagan',
+    Status5: 'Topshiriq berilmagan',
+    CONTESTS: {
+      STANDINGS: 'Standings',
+      CONTESTANT: 'Contestant',
+      PENALTIES: 'Jarimalar',
+      TITLE: 'Nomi',
+      START_TIME: 'Boshlanish vaqti',
+      FINISH_TIME: 'Tugash vaqti',
+      CONTESTANTS_COUNT: 'Ishtirokchilar soni',
+      PROBLEMS_COUNT: 'Masalalar soni',
+      CATEGORY: 'Toifa',
+      POINTS: 'Natija',
     },
   }
 };
--- src/app/i18n/ru.ts
+++ src/app/i18n/ru.ts
@@ -1,15 +1,36 @@
 export const locale = {
-  lang: 'ru',
+  lang: 'uz',
   data: {
     Menu: {
       Home: 'Главная',
-      Problems: 'Проблемы',
+      Problems: 'Задачи',
+      Contests: 'Соревнования',
     },
     Login: 'Войти',
     Logout: 'Выйти',
     PageTitle: {
-      Home: 'Главная',
-      Problems: 'Проблемы',
+      Home: 'Главная страница',
+    },
+    LoginText: 'Iltimos avtorizatsiyadan oʻting va sayohatni davom ettiring',
+    WelcomePage: 'Xush kelibsiz',
+    LoginSuccessText: 'Tizimga muvaffaqiyatli kirdingiz',
+    SolvedProblems: 'Ishlangan masalalar',
+    Status1: 'Ishlagan',
+    Status2: 'Ishlolmagan',
+    Status3: 'Urinish qilmagan',
+    Status4: 'Tizimga kirmagan',
+    Status5: 'Topshiriq berilmagan',
+    CONTESTS: {
+      STANDINGS: 'Результаты',
+      CONTESTANT: 'Участник',
+      PENALTIES: 'Jarimalar',
+      TITLE: 'Nomi',
+      START_TIME: 'Boshlanish vaqti',
+      FINISH_TIME: 'Tugash vaqti',
+      CONTESTANTS_COUNT: 'Ishtirokchilar soni',
+      PROBLEMS_COUNT: 'Masalalar soni',
+      CATEGORY: 'Toifa',
+      POINTS: 'Natija',
     },
   }
 };
